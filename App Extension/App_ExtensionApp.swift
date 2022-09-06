5d84e373637831565147f41214c49b12ba0d3a70
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-06 14:01:42 +0300

    Initial Commit

---
+++ App Extension/App_ExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ExtensionApp.swift
+//  App Extension
+//
+//  Created by Александр on 06.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct App_ExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
