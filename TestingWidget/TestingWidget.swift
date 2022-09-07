f7ced41b74152bc4432156e53ccbfdf6c13e7ed6
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-07 13:38:52 +0300

    TestingWidgetView is added

--- TestingWidget/TestingWidget.swift
+++ TestingWidget/TestingWidget.swift
@@ -42,7 +42,7 @@ struct TestingWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        TestingWidgetView()
     }
 }
 
---
+++ TestingWidget/TestingWidgetView.swift
@@ -0,0 +1,21 @@
+//
+//  TestingWidgetView.swift
+//  App Extension
+//
+//  Created by Александр on 07.09.2022.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct TestingWidgetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestingWidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestingWidgetView()
+    }
+}
