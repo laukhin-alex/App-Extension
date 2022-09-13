3c0e8a12fbdcfe7690c64316080801a5c2e5a6f8
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-13 11:37:49 +0300

    suffixArray was added

---
+++ App Extension/Additions/Extensions.swift
@@ -0,0 +1,43 @@
+//
+//  Extensions.swift
+//  App Extension
+//
+//  Created by Александр on 13.09.2022.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+extension Array: RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Element].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
+
+
